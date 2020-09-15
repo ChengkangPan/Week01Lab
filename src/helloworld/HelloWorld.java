b5e4f5584ae8d5767e34d226ae9db62fdbb7d3ce
832676 <832676@SAIT224432IT.ACDM.DS.SAIT.CA>
2020-09-15 10:31:08 -0600

    New Project Creation Created a new Java Application for HelloWorld

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package helloworld;
 import javax.swing.JOptionPane;
-
 /**
  *
  * @author 832676
  */
 public class HelloWorld {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"Hello World.");
     }
